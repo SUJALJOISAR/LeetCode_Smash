bc99a2fd34f192be242dc372a68d72f00a0e51aa
JOISAR SUJAL <110339249+SUJALJOISAR@users.noreply.github.com>
2023-02-06 22:49:07 +0530

    Added solution - LeetHub

---
+++ Kadane's Algorithm - GFG/kadanes-algorithm.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        int maxi=0,maximum,s=0;
+        maximum=*max_element(arr,arr+n);
+        if(maximum<0)
+        return maximum;
+        else
+        {
+            for(int i=0;i<n;i++)
+            {
+                s+=arr[i];
+                if(s<0)
+                    s=0;
+                maxi=max(maxi,s);
+            }
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+
+// } Driver Code Ends
