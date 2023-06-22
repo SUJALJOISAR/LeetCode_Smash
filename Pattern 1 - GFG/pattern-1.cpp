3ce95530dbb9d25af3bdad12a5001f92c3b48780
JOISAR SUJAL <110339249+SUJALJOISAR@users.noreply.github.com>
2023-06-22 19:43:51 +0530

    Added solution - LeetHub

---
+++ Pattern 1 - GFG/pattern-1.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printSquare(int n) {
+        // code here
+        for (int i = 0; i < n; i++)
+    {
+        // This is the inner loop which here, loops for the columns
+        // as we have to print a rectangular pattern.
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+        }
+       
+        // As soon as N stars are printed, we move to the
+        // next row and give a line break otherwise all stars
+        // would get printed in 1 line.
+        cout << endl;
+    }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printSquare(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
